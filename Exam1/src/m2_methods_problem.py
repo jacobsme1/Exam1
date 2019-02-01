35c333e63210de40d3599b068206154f68aa0a8e
Max Jacobs <jacobsme@rose-hulman.edu>
2019-02-01 15:21:21 -0500

    All todo's done

--- Exam1/src/m2_methods_problem.py
+++ Exam1/src/m2_methods_problem.py
@@ -138,7 +138,7 @@ def count_primes(m,n):
 
 def test_multiply_primes():
     ################################################################################
-    #   TODO: 6. Read the doc string for multiply_primes, below, then
+    #   DONE: 6. Read the doc string for multiply_primes, below, then
     #       then read the test cases here.
     #   After you have read both, change this _TODO_ to DONE.
     ###############################################################################
@@ -183,9 +183,17 @@ def multiply_primes(m,n):
        #     since 2*3*5*7 is 210
        """
     ################################################################################
-    #   TODO: 6. Write the function multiply_primes(m,n) below here
+    #   DONE: 6. Write the function multiply_primes(m,n) below here
     # After you have coded and tested, change this _TODO_ to DONE.
     ###############################################################################
+    product = 1
+    for k in range(m, n+1):
+        if is_prime(k) == True:
+            product = (product) * k
+    if count_primes(m, n) == 0:
+            product = 0
+
+    return product
 
 
 main()
